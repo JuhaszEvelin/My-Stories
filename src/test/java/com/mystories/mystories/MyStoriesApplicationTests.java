41ee77ea2539f458d6106539d74cf097e6c70394
Juhasz Evelin <juhaaszevelin@gmail.com>
2021-03-19 18:18:30 +0100

    jetty plugin implemented, need to be bugged

---
+++ src/test/java/com/mystories/mystories/MyStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystories.mystories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
